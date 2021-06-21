72a4652697093edd87648bf9b8c95add0b5756de
yeongcoder <syj10433@gmail.com>
2021-06-21 14:29:10 +0900

    add exection & write annotation

--- index.js
+++ index.js
@@ -1,70 +1,98 @@
 /*eslint-disable */
 const _ = require("lodash");
-const { ServiceSchemaError } = require("moleculer").Errors;
+const { MoleculerServerError } = require("moleculer").Errors;
 const IgniteClient = require("apache-ignite-client");
 const mybatisMapper = require("mybatis-mapper");
 
 const IgniteClientConfiguration = IgniteClient.IgniteClientConfiguration;
-const CacheConfiguration = IgniteClient.CacheConfiguration;
 const SqlFieldsQuery = IgniteClient.SqlFieldsQuery;
-const QueryEntity = IgniteClient.QueryEntity;
-const QueryField = IgniteClient.QueryField;
 
 class IgniteAdapter {
+  /**
+   * Creates an instance of IgniteAdapter.
+   * @param {Object} opts
+   *
+   * @memberof IgniteAdapter
+   */
   constructor(opts) {
     this.opts = opts;
     this.mapper = _.cloneDeep(mybatisMapper);
   }
+
   /**
    * Initialize adapter
    *
    * @param {ServiceBroker} broker
    * @param {Service} service
    *
-   * @memberof MariaAdapter
+   * @memberof IgniteAdapter
    */
   init(broker, service) {
     this.broker = broker;
     this.service = service;
   }
 
+  /**
+   * Connect to database
+   *
+   * @returns {Promise}
+   *
+   * @memberof IgniteAdapter
+   */
   connect() {
+    if (!this.service.schema.settings.mapperDir) {
+      throw new MoleculerServerError(
+        "Missing `mapperDir` definition in schema.settings of service!"
+      );
+    }
     this.client = new IgniteClient((state, reason) => {
       this.service.logger.info("Ignite StateChanged", state, reason);
     });
     this.configuration = new IgniteClientConfiguration(this.opts.host)
       .setUserName(this.opts.user) // https://ignite.apache.org/docs/latest/security/authentication
-      .setPassword(this.opts.pwd) // https://www.bswen.com/2018/10/java-How-to-solve-Apache-ignite-IgniteException-Can-not-perform-the-operation-because-the-cluster-is-inactive.html
-      .setConnectionOptions(
-        false,
-        {
-          timeout: Number(this.opts.timeout),
-        },
-        true
-      ); // https://ignite.apache.org/docs/latest/thin-clients/nodejs-thin-client#partition-awareness
+      .setPassword(this.opts.password) // https://www.bswen.com/2018/10/java-How-to-solve-Apache-ignite-IgniteException-Can-not-perform-the-operation-because-the-cluster-is-inactive.html
+      .setConnectionOptions(this.opts.useTLS, this.opts.connectionOptions); // https://ignite.apache.org/docs/latest/thin-clients/nodejs-thin-client#partition-awareness
     this.mapper.createMapper([this.service.schema.settings.mapperDir]);
     return this.client
       .connect(this.configuration)
-      .then(() => this.client.getCache(this.opts.cache))
+      .then(() => this.client.getOrCreateCache(this.opts.cache))
       .then((cache) => {
         this.db = cache;
-        this.db.queryExecute = this.queryExecute.bind(this);
+        this.db.sendQuery = this.sendQuery.bind(this);
         this.service.logger.info("Ignite adapter has connected successfully.");
       });
   }
 
+  /**
+   * Disconnect from database
+   *
+   * @returns {void}
+   *
+   * @memberof IgniteAdapter
+   */
   disconnect() {
-    return this.client.disconnect();
+    if (this.client) {
+      this.client.disconnect();
+    }
+    return Promise.resolve();
   }
 
-  async queryExecute(id, params) {
+  /**
+   * Send SQL Query to Database
+   *
+   * @param {string} id
+   * @param {object?} params
+   * @returns {Promise}
+   *
+   * @memberof IgniteAdapter
+   */
+  async sendQuery(id, params) {
     const sql = this.mapper.getStatement("ignite", id, params);
     this.service.logger.info(sql);
     try {
       let results = [];
       const sqlFieldsQuery = new SqlFieldsQuery(sql).setIncludeFieldNames(true);
       const cursor = await this.db.query(sqlFieldsQuery);
-      this.service.logger.info("sqlFieldsQuery: ", sqlFieldsQuery);
       this.service.logger.info("cursor: ", cursor);
 
       do {
